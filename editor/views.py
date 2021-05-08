bccdfba19c66429ea3049778befb5540e5438dd8
Sayeed Md. Shaiban <sayeed.k06@gmail.com>
2021-05-08 16:02:50 +0600

    added edit functionality and edit button routing

--- editor/views.py
+++ editor/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import TextData
 from .forms import TextDataForm
+from django.http import Http404
 import uuid
 # Create your views here.
 """This function accepts form submission from a button(name='text_submission'),
@@ -32,19 +33,28 @@ def show(request, unique_id=None):
         'unique_id': text_info.text_url_id,
         'text': text_info.text
     })
-    except ValueError:
-        print('Something wronmg')
-        return render(request, 'editor/editor.html', {
-        'unique_id': unique_id,
-    })
+    except TextData.DoesNotExist:
+        raise Http404("No TextData matches the given query.")
+        
 
 """This function gets the unique id from url. Then uses the unique id to find the 
 text field with matching unique id and displayes it to the user. And then lets them edit 
 the existing text and save new text in the database with another unique id
    URL: '/edit/<unique_id>' """
 def edit(request, unique_id=None):
-    if unique_id:
-        print(unique_id)
+    if "text_submission" in request.POST:
+        unique_id = uuid.uuid1()
+        incoming_data = TextDataForm({
+            'text': request.POST['text'],
+            'text_url_id': unique_id
+        })
+
+        if incoming_data.is_valid():
+            incoming_data.save()
+
+        return redirect('show_text', unique_id=unique_id)
+    elif unique_id:
+        text_info = TextData.objects.get(text_url_id=unique_id)
         return render(request, 'editor/edit_text.html', {
-            'prev_text': unique_id 
+            'prev_text': text_info.text 
         })
