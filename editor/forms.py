0bb9e4fba0dbcf5f0ad7259b51a54269825537ab
Sayeed Md. Shaiban <sayeed.k06@gmail.com>
2021-05-26 10:30:17 +0600

    added history tracking functionality

--- editor/forms.py
+++ editor/forms.py
@@ -1,6 +1,10 @@
-from .models import TextData
+from .models import TextData,TextHist
 from django import forms
 class TextDataForm(forms.ModelForm):
     class Meta:
         model = TextData
         fields = ('text', 'text_url_id')
+class TextHisForm(forms.ModelForm):
+    class Meta:
+        model = TextHist
+        fields = '__all__'
--- editor/views.py
+++ editor/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,redirect
-from .models import TextData
-from .forms import TextDataForm
+from .models import TextData, TextHist
+from .forms import TextDataForm,TextHisForm
 from django.http import Http404
 import uuid
-
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+from pygments.styles import get_style_by_name
 # creates a random id. If random id already exists, recursively creates another random id.
 def random_id_gen():
     unique_code = uuid.uuid1()
@@ -12,7 +15,13 @@ def random_id_gen():
     else:
         return unique_code 
 
-
+def findhis(id):
+    list_his = []
+    while(TextHist.objects.filter(current_text=id)):
+        model = TextHist.objects.get(current_text=id)
+        list_his.append(model.prev_text)
+        id = model.prev_text
+    return list_his
 """This function accepts form submission from a button(name='text_submission'),
 creates a unique id for the text and saves the information in the database after form validation 
 URL: '/' """
@@ -37,9 +46,17 @@ URL: '/<unique_id>' """
 def show(request, unique_id=None):
     try:
         text_info = TextData.objects.get(text_url_id=unique_id)
+        lexer = get_lexer_by_name("python", stripall=True)
+        style = get_style_by_name('friendly')
+        formatter = HtmlFormatter(style=style,linenos='inline',nobackground=True,lineseparator='<br>')
+        formatted = highlight(text_info.text, lexer, formatter)
+
+        history = findhis(unique_id)
+
         return render(request, 'editor/editor.html', {
         'unique_id': text_info.text_url_id,
-        'text': text_info.text
+        'text': formatted,
+        'hist': history,
     })
     except TextData.DoesNotExist:
         raise Http404("No TextData matches the given query.")
@@ -55,19 +72,33 @@ the existing text and save new text in the database with another unique id
 URL: '/edit/<unique_id>' """
 def edit(request, unique_id=None):
     if "text_submission" in request.POST:
+        print(unique_id)
         unique_id = random_id_gen()
         
         incoming_data = TextDataForm({
             'text': request.POST['text'],
             'text_url_id': unique_id
         })
-
+        print("prev_text", request.POST['prev_id'])
+        print("Current_text", unique_id)
+        form = TextHisForm({
+                'prev_text': request.POST['prev_id'],
+                'current_text': unique_id
+            })
         if incoming_data.is_valid():
             incoming_data.save()
+            
+        if form.is_valid():
+            form.save()
+        else:
+            print("This didn't work")
 
         return redirect('show_text', unique_id=unique_id)
     elif unique_id:
         text_info = TextData.objects.get(text_url_id=unique_id)
+        history = findhis(unique_id)
         return render(request, 'editor/edit_text.html', {
-            'prev_text': text_info.text 
+            'prev_text': text_info.text,
+            'prev_url_id': unique_id,
+             'hist': history
         })
