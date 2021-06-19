d374f8afc23f487a4562f4e7fc7f75acc01b8ccb
Sayeed Md. Shaiban <sayeed.k06@gmail.com>
2021-06-19 23:22:33 +0600

    key change

--- D2_textEditor/settings.py
+++ D2_textEditor/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&5gtxzvs7pa1%ok0i-bw4y(fe%h@+3j26_my*5^c2ik@7r5-o&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
